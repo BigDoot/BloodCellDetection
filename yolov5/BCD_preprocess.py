2a267afb462d018ca3464775af6de1a5d0948cc5
BigDoot <glendonchua@gmail.com>
2022-01-22 21:30:19 +0800

    Modify dataset split

--- yolov5/BCD_preprocess.py
+++ yolov5/BCD_preprocess.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import os
+from sklearn.model_selection import train_test_split
+import shutil
+
 '''import torch
 from IPython.display import Image  # for displaying images
 import os 
 import random
 import shutil
-from sklearn.model_selection import train_test_split
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 from tqdm import tqdm
@@ -19,17 +21,16 @@ import matplotlib.pyplot as plt'''
 data = pd.read_csv('annotations.csv',engine='python')
 
 # Display 5 random samples
-print(data.sample(5))
+#print(data.sample(5))
 
 # Split data into train and test sets in 8:2 ratio
 images = data.image.unique()
-train = images[:80]
-test = images[80:]
-print(test)
+#train = images[:80]
+#test = images[80:]
+#print(test)
 d = data[data['image'].isin({'image-25.png'})]
-print(d.image.values[0])
-#for i in d:
-#    print(d[i.values)
+#print(d.image.values[0])
+
 
 
 def processData(data):
@@ -87,8 +88,39 @@ def convert_to_yolov5(info_dict):
     # Save the annotation to disk
     print("\n".join(print_buffer), file= open(save_file_name, "w"))
     
+# Convert all annotations to YOLOv5 annotation format
 
-for image in train:
+for image in images:
     d = data[data['image'].isin({image})]
     convert_to_yolov5(processData(d))
 
+# Read images and annotations
+images = [os.path.join('images', x) for x in os.listdir('images') if x[-3:] == "png"]
+annotations = [os.path.join('annotations', x) for x in os.listdir('annotations') if x[-3:] == "txt"]
+
+images.sort()
+print(images)
+annotations.sort()
+
+# Split the dataset into train-valid-test splits 
+train_images, val_images, train_annotations, val_annotations = train_test_split(images, annotations, test_size = 0.2, random_state = 1)
+val_images, test_images, val_annotations, test_annotations = train_test_split(val_images, val_annotations, test_size = 0.5, random_state = 1)
+
+print(val_images)
+
+#Utility function to move images 
+def move_files_to_folder(list_of_files, destination_folder):
+    for f in list_of_files:
+        try:
+            shutil.move(f, destination_folder)
+        except:
+            print(f)
+            assert False
+
+# Move the splits into their folders
+move_files_to_folder(train_images, 'images/train')
+move_files_to_folder(val_images, 'images/val/')
+move_files_to_folder(test_images, 'images/test/')
+move_files_to_folder(train_annotations, 'annotations/train/')
+move_files_to_folder(val_annotations, 'annotations/val/')
+move_files_to_folder(test_annotations, 'annotations/test/')
